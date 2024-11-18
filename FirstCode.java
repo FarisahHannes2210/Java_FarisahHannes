c3e42c27ca0e433de5b30b4a142fbc479dc1433b
FarisahHannes2210 <Hannesfarisah@gmail.com>
2024-11-18 12:01:32 +0800

    add comment

--- FirstCode.java
+++ FirstCode.java
@@ -3,6 +3,6 @@
 public class FirstCode {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); //this line is not vibingg :(
     }
 }
